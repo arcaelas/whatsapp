f51b081d7e9d5f144d15c07bf49d9d4a7fd4e929
Miguel Guevara <arcaela.reyes@gmail.com>
2025-07-25 19:15:25 -0400

    docs: agregar JSDoc a la clase Base y sus métodos de serialización

--- src/model/base.ts
+++ src/model/base.ts
@@ -4,8 +4,18 @@ import WhatsApp from 'src';
 
 type Serialize<T> = { [K in keyof T as T[K] extends Noop ? never : K]: T[K] };
 
+/**
+ * @module Base
+ * @description
+ * Base class for WhatsApp entities.
+ */
 export default class Base<T> {
-    constructor(protected readonly $: WhatsApp<'code' | 'qr'>, protected readonly _: Serialize<T>) { }
+    constructor(protected readonly $: WhatsApp<'code' | 'qr'>, protected readonly _: Serialize<T>) {}
+    /**
+     * @description
+     * Returns a JSON representation of this entity.
+     * @returns A JSON representation of this entity.
+     */
     toJSON() {
         const o: any = {};
         for (const k in this) {
@@ -14,6 +24,11 @@ export default class Base<T> {
         }
         return o;
     }
+    /**
+     * @description
+     * Returns a string representation of this entity.
+     * @returns A string representation of this entity.
+     */
     toString() {
         return JSON.stringify(this.toJSON(), Baileys.BufferJSON.replacer);
     }
